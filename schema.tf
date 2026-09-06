d35fee93c0c35c867d5ba20c2afc418c3278879d
Ajith Vijayan <vihaanajith1989@gmail.com>
2026-09-06 11:31:08 +0530

    This is part of my Terraform testing

--- schema.tf
+++ schema.tf
@@ -4,3 +4,9 @@ resource "snowflake_schema" "sales" {
 
   comment = "Sales schema managed by Terraform"
 }
+resource "snowflake_schema" "customer" {
+  database = snowflake_database.demo_db.name
+  name     = "CUSTOMER"
+
+  comment = "Customer schema managed by Terraform"
+}
